2a889dd9d8a9e4bbe3a80a36c429fb792c032e1e
amirjaf <ajafari@dvrpc.org>
2025-01-16 14:06:31 -0500

    - add trip data loader function - change from CSV to pkl to speed up the reading process

--- src/utils/data_loader.py
+++ src/utils/data_loader.py
@@ -10,10 +10,22 @@ import os
 
 def get_tour_data():
     cwd = os.getcwd()
-    data_path = os.path.join(cwd, 'src', 'assets', 'data', 'tour_data_processed_0701.csv')
+    data_path = os.path.join(cwd, 'src', 'assets', 'data', 'tour_data_processed_0701.pkl')
     needed_columns = ['RACE', 'tourmode', 'psexpfac', 'pdpurp2', 'pdpurp', 'ocounty', 'HISP_B', 'lowinc','distcat', 'tautodist','distcat', 'tourmode2', 'timecat2', 'ttravtime']
-    df = pandas.read_csv(data_path, usecols=needed_columns, memory_map=True)
+    df = pandas.read_pickle(data_path)
+    df = df[needed_columns] # make sure all columns are available
     df = df[df['tourmode']!=0] # remove 'other' mode
+    df['lowinc'] = df['lowinc'] + 1 # I need my categories to start from 1. to be able to get the correct title name for the graphs. 
+    df['HISP_B'] = df['HISP_B'] + 1 # I need my categories to start from 1. to be able to get the correct title name for the graphs. 
+    return df
+
+def get_trip_data():
+    cwd = os.getcwd()
+    data_path = os.path.join(cwd, 'src', 'assets', 'data', 'trip_data_processed_0701.pkl')
+    needed_columns = ['RACE', 'tripmode', 'psexpfac', 'dpurp2', 'dpurp', 'ocounty', 'HISP_B', 'lowinc','distcat', 'travdist','distcat', 'tripmode2', 'timecat2', 'ttravtime']
+    df = pandas.read_pickle(data_path)
+    df = df[needed_columns] # make sure all columns are available
+    df = df[df['tripmode']!=0] # remove 'other' mode
     df['lowinc'] = df['lowinc'] + 1 # I need my categories to start from 1. to be able to get the correct title name for the graphs. 
     df['HISP_B'] = df['HISP_B'] + 1 # I need my categories to start from 1. to be able to get the correct title name for the graphs. 
     return df
