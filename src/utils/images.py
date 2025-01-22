adc76d1b74262b83775f8c92219c1c3bf6d42893
amirjaf <ajafari@dvrpc.org>
2025-01-22 15:41:32 -0500

    style: remove extra line of codes

--- src/utils/images.py
+++ src/utils/images.py
@@ -8,9 +8,6 @@ I suggest handling the local file encoding/decoding here as well as fetching any
 import base64
 import os
 
-# image CDNs
-image_cdn = 'https://images.dog.ceo/breeds'
-
 # logo information - dvrpc logo
 cwd = os.getcwd()
 logo_path = os.path.join(cwd, 'src', 'assets', 'logos', '1a_ColorPrimarySmall.png')
@@ -22,11 +19,4 @@ with open(logo_path_equity_dash, 'rb') as logo_file:
     logo_tunel_equity_dash = base64.b64encode(logo_file.read())
 logo_encoded_equity_dash = 'data:image/svg+xml;base64,{}'.format(logo_tunel_equity_dash.decode())
 
-def get_dog_image(breed, name):
-    '''
-    This method assumes that you are fetching specific images hosted on a CDN.
-    For instance, random dog pics given a breed.
-    '''
-    if breed and name:
-        return f'{image_cdn}/{breed}/{name}.jpg'
-    return None
+
