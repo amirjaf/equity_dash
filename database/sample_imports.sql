68fd4f6a43a48abeac818efa0139ec5b022f2e34
amirjaf <ajafari@dvrpc.org>
2025-02-27 17:10:37 -0500

    add db

---
+++ database/sample_imports.sql
@@ -0,0 +1,2 @@
+SELECT import_tour_table('C:\dash_data\tour_data_processed_0701.csv', 'tour_data_processed_0701', 'equity');
+SELECT import_trip_table('C:\dash_data\trip_data_processed_0701.csv', 'trip_data_processed_0701', 'equity');
